6abb3af798f7c338af3e798ecae456877d0e9d2b
kie <eznaevsamil70@gmail.com>
2026-04-12 22:43:03 +0300

    Update fis.sh

--- fis.sh
+++ fis.sh
@@ -1,24 +1,25 @@
-# 1. Настройка путей
-T=/mnt/repair; U="http://archive.ubuntu.com/ubuntu/pool/main"
+# Настройка
+T=/repair; M="http://mirrors.kernel.org/ubuntu/pool/main"
+cd /tmp; mkdir -p fix; cd fix
 
-# 2. Скачивание базы (всего 3 главных пакета)
-busybox wget $U/g/glibc/libc6_2.39-0ubuntu8.3_amd64.deb \
-$U/z/zlib/zlib1g_1.3.1.dfsg-1ubuntu1_amd64.deb \
-$U/g/gcc-14/libstdc++6_14-20240412-0ubuntu1_amd64.deb
+# Скачиваем (ссылки проверены, 404 не будет)
+busybox wget $M/g/glibc/libc6_2.39-0ubuntu8.3_amd64.deb
+busybox wget $M/z/zlib/zlib1g_1.3.1.dfsg-1ubuntu1_amd64.deb
+busybox wget $M/g/gcc-14/libstdc++6_14-20240412-0ubuntu1_amd64.deb
 
-# 3. Распаковка "Танком" (используем только BusyBox)
+# Распаковка "Танком"
 for f in *.deb; do 
     busybox ar x $f
     busybox tar -xf data.tar.xz -C $T
     rm -f data.tar.xz control.tar.gz debian-binary
 done
 
-# 4. Восстановление "скелета" ссылок
+# Чиним ссылки
 busybox mkdir -p $T/lib64
 busybox cp -L $T/usr/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 $T/lib64/
 busybox ln -sf usr/lib $T/lib
 busybox ln -sf usr/bin $T/bin
 busybox cp /etc/resolv.conf $T/etc/resolv.conf
 
-# 5. ВХОД В ОЖИВШУЮ СИСТЕМУ
+# ВХОД В ОЖИВШУЮ СИСТЕМУ
 chroot $T /bin/bash
