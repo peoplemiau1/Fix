0fc697d711728d396b8d479d70642a7559bc34df
kie <eznaevsamil70@gmail.com>
2026-04-13 14:01:21 +0300

    Update fis.sh

--- fis.sh
+++ fis.sh
@@ -1,1 +1,9 @@
-wget https://ru.releases.ubuntu.com/ubuntu-cdimage/ubuntu-base/releases/24.04.1/release/ubuntu-base-24.04.3-base-amd64.tar.gz
+cd /tmp && \
+wget -q http://mirror.yandex.ru/ubuntu/pool/main/u/util-linux/libblkid1_2.39.3-9ubuntu6.5_amd64.deb && \
+wget -q http://mirror.yandex.ru/ubuntu/pool/main/u/util-linux/libmount1_2.39.3-9ubuntu6.5_amd64.deb && \
+wget -q http://mirror.yandex.ru/ubuntu/pool/main/u/util-linux/libuuid1_2.39.3-9ubuntu6.5_amd64.deb && \
+for f in lib*{blkid,mount,uuid}1_*.deb; do 
+    echo "Распаковываем $f ..." && 
+    sudo dpkg-deb -x "$f" / 
+done && \
+echo "✅ Готово! Установлена версия 2.39.3-9ubuntu6.5 (актуальная на апрель 2026)"
